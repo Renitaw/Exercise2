dac93078ffb6365c83c94a6e218b87f25bd87ab0
R Widiastuti <r.widiastuti.ft21@mail.umy.ac.id>
2022-11-03 16:26:08 +0700

    creating a method to sort array and using insertion sort

--- Exercise2/Program.cs
+++ Exercise2/Program.cs
@@ -9,7 +9,7 @@ namespace Exercise2
     class program
     {
         //deklarasi array and variabel 
-        private int[] Renita = new int[30];
+        private int[] Renita = new int[34];
         int n;
         int i;
 
@@ -22,12 +22,37 @@ namespace Exercise2
                 Console.WriteLine("Enter  the number of elements in the array : ");
                 string s = Console.ReadLine();
                 n = Int32.Parse(s);
-                if ((n > 0) && (n <= 30))
+                if ((n > 0) && (n <= 34))
                     break;
                 else
                     Console.WriteLine("\nArray should have a minimum of 1 and a maximum of 30 elements. \n");
             }
+            Console.WriteLine("");
+            Console.WriteLine("====================");
+            Console.WriteLine("Enter array elements");
+            Console.WriteLine("====================");
+            for (i = 0; i < n; i++)
+            {
+                Console.Write("<" + (i + 1) + ">");
+                string s1 = Console.ReadLine();
+                Renita[i] = Int32.Parse(s1);
+            }
         }
+        //creating a method to sort array using insertion sort
+        public void insertionsort()
+        {
+            for (int i = 0; i < n; i++)
+            {
+                int temp = Renita[i];
+                int RW = i - i;
+                while (RW >= 0 && Renita[RW] > temp)
+                {
+                    Renita[RW + 1] = Renita[RW];
+                    RW = RW - 1;
+                }
+            }
+        }
+       
     }
 
 
