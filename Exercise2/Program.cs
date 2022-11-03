b971cbbe495f9b74642233553e1c52f0ac3cdf6c
R Widiastuti <r.widiastuti.ft21@mail.umy.ac.id>
2022-11-03 16:03:43 +0700

    membuat method dan memasukkan input

--- Exercise2/Program.cs
+++ Exercise2/Program.cs
@@ -12,6 +12,22 @@ namespace Exercise2
         private int[] Renita = new int[30];
         int n;
         int i;
+
+        //creating a method for receiving input
+
+        public void input()
+        {
+            while (true)
+            {
+                Console.WriteLine("Enter  the number of elements in the array : ");
+                string s = Console.ReadLine();
+                n = Int32.Parse(s);
+                if ((n > 0) && (n <= 30))
+                    break;
+                else
+                    Console.WriteLine("\nArray should have a minimum of 1 and a maximum of 30 elements. \n");
+            }
+        }
     }
 
 
